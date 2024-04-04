59fd06d2a9e6c56ddfa8cdee3bed1b9fdb3f02f5
Peiyi Li <pli11@ncsu.edu>
2024-04-04 00:07:45 -0400

    add file to save ibm account info

---
+++ save_ibm_account_info.py
@@ -0,0 +1,6 @@
+from qiskit_ibm_provider import IBMProvider
+#save the account info
+#token: IBM Quantum API token.
+#instance: The hub/group/project.
+IBMProvider.save_account(token = 'insert_token_here',instance = 'ibm-q/open/main')
+
